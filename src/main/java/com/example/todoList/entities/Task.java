6a921735cf3753a930912cf3df8d614c1f667d47
Vinicius Lacerda <vini.lacerda@hotmail.com>
2025-10-05 14:12:46 -0300

    feat(Task): Entity created. One to many and Many to one mapped

---
+++ src/main/java/com/example/todoList/entities/Task.java
@@ -0,0 +1,83 @@
+package com.example.todoList.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+@Table(name = "task")
+public class Task implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+    private String description;
+    private String status;
+    private Instant createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Task(Long id, String title, String description, String status, Instant createdAt, User user) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.user = user;
+    }
+
+    public Task() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
